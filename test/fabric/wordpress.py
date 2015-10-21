8b5ff503487cdb3cf2683998d79e8141937f1d2d
Chris Hultin <chris.hultin@rackspace.com>
2015-10-21 14:44:02 -0500

    Updating CircleCI tests to no longer check for the presence of MySQL

--- test/fabric/wordpress.py
+++ test/fabric/wordpress.py
@@ -12,7 +12,6 @@ def check():
 
     assert port.is_listening(21), 'port 21/vsftpd is not listening'
     assert port.is_listening(80), 'port 80/varnishd is not listening'
-    assert port.is_listening(3306), 'port 3306/mysqld is not listening'
     assert port.is_listening(6082), 'port 6082/varnishd is not listening'
     assert port.is_listening(8080), 'port 8080/apache2 is not listening'
     assert port.is_listening(11211), 'port 11211/memcached is not listening'
@@ -20,19 +19,16 @@ def check():
     assert user.exists('ftp'), 'ftp user does not exist'
     assert user.exists('varnish'), 'varnish user does not exist'
     assert user.exists('varnishlog'), 'varnishlog user does not exist'
-    assert user.exists('mysql'), 'mysql user does not exist'
     assert user.exists('memcache'), 'memcache user does not exist'
     assert user.exists('wp_user'), 'wp_user user does not exist'
     assert user.exists('wp_user').get('passwd') != '!', 'wp_user pass missing'
 
     assert process.is_up('apache2'), 'apache2 is not running'
-    assert process.is_up('mysqld'), 'mysqld is not running'
     assert process.is_up('varnishd'), 'varnishd is not running'
     assert process.is_up('memcached'), 'memcached is not running'
     assert process.is_up('vsftpd'), 'vsftpd is not running'
 
     assert service.is_enabled('apache2'), 'apache2 service not enabled'
-    assert service.is_enabled('mysql'), 'mysql service not enabled'
     assert service.is_enabled('varnish'), 'varnish service not enabled'
     assert service.is_enabled('varnishlog'), 'varnishlog service not enabled'
     assert service.is_enabled('memcached'), 'memcached service not enabled'
